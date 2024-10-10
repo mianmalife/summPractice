cd122d141b7d7ebefe2b40d7eb8645ddf8db284a
mianmalife <253590433@qq.com>
2024-10-10 22:20:19 +0800

    feat: 代理_data [skip]

--- vue/state.js
+++ vue/state.js
@@ -19,11 +19,25 @@ export function initState(vm) {
   }
 }
 
+function proxy(vm, data, key) {
+  Object.defineProperty(vm, key, {
+    get() {
+      return vm[data][key]
+    },
+    set(newValue) {
+      vm[data][key] = newValue
+    }
+  })
+}
+
 function initProps() { }
 function initMethods() { }
 function initData(vm) {
   let data = vm.$options.data
   data = vm._data = typeof data === 'function' ? data.call(vm) : data || {}
+  Object.keys(data).forEach(key => {
+    proxy(vm, '_data', key)
+  })
   observe(data)
 }
 function initComputed() { }
