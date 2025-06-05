1362a5daf9d669c6f69c16d8e3d2fb70d22e3725
mianmalife <253590433@qq.com>
2025-06-05 16:36:58 +0800

    docs: 设置提交用户信息

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -45,7 +45,7 @@ export default defineConfig({
         text: '推送',
         items: [
           { text: '远程仓库地址设置与推送', link: '/docs/gitOperation/' },
-          { text: '...', link: '/docs/gitOperation/one' }
+          { text: '设置提交用户信息', link: '/docs/gitOperation/one' }
         ]
       }],
       '/docs/nodejs/': [{
--- docs/gitOperation/one.md
+++ docs/gitOperation/one.md
@@ -1,1 +1,23 @@
-...
+# 设置提交用户信息
+
+## 设置当前项目的提交用户信息
+
+```bash
+git config user.name 'xxx'
+
+git config user.email 'xxx'
+```
+
+## 设置全局提交用户信息
+
+```bash
+git config --global user.name 'xxx'
+
+git config --global user.email 'xxx'
+```
+
+## 查看当前配置信息
+
+```bash
+git config --list
+```
